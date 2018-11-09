f94082be63f0d7f69ff4a19643e43c22e45ed002
michitaro.koike <michitaro.nike@gmail.com>
2018-11-09 10:58:22 +0900

    fix

--- centos7/setup.bash
+++ centos7/setup.bash
@@ -46,7 +46,7 @@ sudo yum -y install ncurses-devel libevent-devel
 (
     sudo wget -O /etc/yum.repos.d/virtualbox.repo \
         https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
-    virtualbox_package=$(sudo yum search VirtualBox | grep -E '^VirtualBox-5' | tail -1 | cut -d\  -f1)
+    virtualbox_package=$(sudo yum search -y VirtualBox | grep -E '^VirtualBox-5' | tail -1 | cut -d\  -f1)
     yum -y install $virtualbox_package
 )
 
