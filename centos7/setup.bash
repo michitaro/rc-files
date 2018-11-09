8f9580f5afbfccb90b32d37b8a315a7cddd0f8b9
michitaro.koike <michitaro.nike@gmail.com>
2018-11-09 10:40:37 +0900

    cleanup

--- centos7/setup.bash
+++ centos7/setup.bash
@@ -1,18 +1,19 @@
+base=$(readlink -f $(dirname $0))
+
 set -e
+rm -rf ~/tmp
 
 sudo yum install -y wget vim gcc git automake
 sudo yum -y install ncurses-devel libevent-devel
 
-rm -rf ~/tmp
-
 # tmux
 (
-    mkdir -p ~/tmp
-    cd ~/tmp
+    mkdir -p ~/tmp && cd ~/tmp
     git clone https://github.com/tmux/tmux.git
     cd tmux
     sh autogen.sh
 	./configure --prefix=$HOME/local/packages/tmux && make -j && make install
+    cp $base/tmux/tmux.conf ~/.tmux.conf
 )
 
 # fish
@@ -20,18 +21,25 @@ rm -rf ~/tmp
 	cd /etc/yum.repos.d/
 	sudo wget https://download.opensuse.org/repositories/shells:fish:release:2/CentOS_7/shells:fish:release:2.repo
 	sudo yum install -y fish
+    mkdir -p ~/.config/fish
+    cp -r $base/fish/ ~/.config/fish
 )
 
 # htop
 (
-    mkdir -p ~/tmp
-    cd ~/tmp
+    mkdir -p ~/tmp && cd ~/tmp
 	rm -rf ~/local/packages/htop
 	htop_version=$(curl https://hisham.hm/htop/releases/ | grep '<a' | tail -1 | perl -ne 'print $1 if m|>(.*)/<|')
 	curl https://hisham.hm/htop/releases/$htop_version/htop-$htop_version.tar.gz | tar xvzf -
 	cd htop-$htop_version
 	./configure --prefix=$HOME/local/packages/htop 
 	make -j && make install
+)
+
+# vim
+(
+    cp $base/vim/vimrc ~/.vimrc
+    mkdir -p ~/.vim-backup
 )
 
 # virtualbox
